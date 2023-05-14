577615d88b5eff7dc675d920fa7af565b8ac8e6a
Alexander <bdiyoz@gmail.com>
2023-05-14 15:21:01 +0300

    [FEAT] timer refactor

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,9 @@
 export const GENERAL_TIME_MS = 2.4e+6;
 export const ONE_MINUTE_MS = 6e+4;
 export const ONE_SEC_MS = 1e+3;
-export const SIXTY = 60;
-export const MINUTE_UNIT_COUNTER = 1;
-export const MINUTE_DECADE_COUNTER = 2;
-export const SECOND_UNIT_COUNTER = 4;
-export const SECOND_DECADE_COUNTER = 5;
-export const SECOND_HUNDRED_COUNTER = 6;
+export const SECOND_UNIT_COUNTER = 1;
+export const SECOND_DECADE_COUNTER = 2;
+export const SECOND_HUNDRED_COUNTER = 3;
 export const LINE_LENGTH = 16;
 export const BOMBS_NUMBER = 16;
 export const BOMB = -1;
@@ -28,11 +25,6 @@ export enum BasicNumbers {
   Seven = 7,
   Eight = 8,
   Nine = 9,
-};
-
-export enum TimerKind {
-  Minutes = 'minutes',
-  Seconds = 'Seconds',
 };
 
 export enum GameStatus {
